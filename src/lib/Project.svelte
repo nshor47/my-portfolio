c37a7374f9db824981d2a1c37322f1e653683bd8
nshor47 <91350878+nshor47@users.noreply.github.com>
2024-11-04 15:24:28 -0800

    Lab 6 Step 0.1 and Project Display changes

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -4,27 +4,48 @@
 </script>
 
 <div class="projects">
-    <svelte:element this={'h' + hLevel}>{data.title}</svelte:element>
-    <article>
+    <article class="project">
+        <svelte:element this={'h' + hLevel} class="project-title">{data.title}</svelte:element>
         <img src={data.image} alt={data.title}/>
+        <div class="description"></div>
         <p>{data.description}</p>
+        <p>c. {data.year}</p>
     </article>
 </div>
 
 <style>
-    .projects{
-        display:grid; */
-        grid-template-columns: repeat(auto-fill, minmax(26em, 1fr));
-        gap: 1em;
+    .projects {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(550px, 1fr));
+        gap: 1.5em;
+        justify-content: center; 
+        padding: 2em; 
+        max-width: 1000px; 
+        margin: 0 auto;
     }
 
     .projects article {
-        display: grid;
-        grid-template-rows: subgrid;
-        grid-row: span 3;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         background-color: var(--color-header-bg);
-        padding: 15px; 
+        padding: 15px;
         border-radius: 8px;
         box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        text-align: center; 
+    }
+
+    .projects img {
+        width: 100%; 
+        max-width: 700px; 
+        height: auto;
+        margin-bottom: 1em;
+        border-radius: 8px;
+    }
+
+    .project-title {
+        font-size: 2em;
     }
 </style>
+
+
