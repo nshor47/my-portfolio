cc9467d4c17c78fdd03b4b080e3c34607de5e3a3
nshor47 <91350878+nshor47@users.noreply.github.com>
2024-11-13 12:49:49 -0800

    Lab 7 Step 2 Finished

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -4,7 +4,10 @@
 
     let data = [];
     let commits = [];
-    let numFiles, avgFileLength, avgLineLength, maxWorkDay, maxPeriod, maxDepth;
+    let numFiles, avgFileLength, avgLineLength, maxWorkDay, maxPeriod;
+    let xScale, yScale;
+    let xAxis, yAxis, yAxisGridlines;
+
 
 
     onMount(async () => {
@@ -54,9 +57,53 @@
 
             const workByPeriod = d3.rollups(data, v => v.length, d => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }));
             maxPeriod = d3.greatest(workByPeriod, d => d[1])?.[0];
+
+            xScale = d3.scaleTime()
+            .domain(d3.extent(commits, d => d.datetime))
+            .range([usableArea.left, usableArea.right])
+            .nice();
+            yScale = d3.scaleLinear()
+            .domain([0, 24])
+            .range([usableArea.bottom, usableArea.top]);
     });
+    
+let width = 1000,
+    height = 600;
+
+let margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+let usableArea = {
+  top: margin.top,
+  right: width - margin.right,
+  bottom: height - margin.bottom,
+  left: margin.left,
+};
+
+usableArea.width = usableArea.right - usableArea.left;
+usableArea.height = usableArea.bottom - usableArea.top;
+
+xScale = d3.scaleTime()
+            .domain(d3.extent(commits, d => d.datetime))
+            .range([usableArea.left, usableArea.right])
+            .nice();
+
+yScale = d3.scaleLinear()
+      .domain([0, 24])
+      .range([usableArea.bottom, usableArea.top]);
+
+$: {
+    d3.select(xAxis).call(d3.axisBottom(xScale)),
+    d3.select(yAxis).call(d3.axisLeft(yScale));
+    d3.select(yAxis).call(
+    d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00'),
+    d3.select(yAxisGridlines).call(
+        d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width),
+    )
+);
+}
 
 </script>
+
 
 <h1>Metadata</h1>
 <h2>Summary</h2>
@@ -75,3 +122,32 @@
     
     <dt>Time of Day with Most Work</dt><dd>{maxPeriod}</dd>
 </dl>
+
+<br>
+<h2>Commits by Time of Day</h2>
+<svg viewBox="0 0 {width} {height}">
+    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+    <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+    <g class="dots">
+        {#each commits as commit, index }
+        <circle
+          cx="{xScale(commit.datetime)}"
+          cy="{yScale(commit.hourFrac)}"
+          r="5"
+          fill="steelblue"
+        />
+        {/each}
+      </g>
+</svg>
+
+
+
+<style>
+    svg {
+      overflow: visible;
+    }
+    .gridlines {
+      stroke-opacity: 0.2;
+    }
+</style>
