0f1f87925d86f66c420a9559dbda65f699867c9d
nshor47 <91350878+nshor47@users.noreply.github.com>
2024-11-20 15:25:45 -0800

    Url update for projects

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -14,6 +14,7 @@
         <img src={data.image} alt={data.title}/>
         <div class="description"></div>
         <p>{data.description}</p>
+        <p>{data.url}</p>
         <p>c. {data.year}</p>
     </article>
 </div>
